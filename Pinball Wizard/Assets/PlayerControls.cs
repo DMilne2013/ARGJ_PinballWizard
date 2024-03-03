d6b0a779e561a2a361c1baa95f959a1c6024fee3
David <davidmilne2013@gmail.com>
2024-03-03 13:33:12 -0600

    Setting up physic materials and basic layout

---
+++ Pinball Wizard/Assets/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public GameObject paddleL;
+    public GameObject paddleR;
+    public GameObject plunger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
